df84d2e854d4df2b15afdef6fbb4d1c5285fd88a
TheQjc <3063751837@qq.com>
2025-09-12 23:47:55 +0800

    用户模块完成

---
+++ src/main/java/com/yipi/yupicturebackend/model/dto/user/UserQueryRequest.java
@@ -0,0 +1,29 @@
+package com.yipi.yupicturebackend.model.dto.user;
+
+import com.yipi.yupicturebackend.common.PageRequest;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.io.Serializable;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+public class UserQueryRequest extends PageRequest implements Serializable {
+
+    
+    private Long id;
+
+    
+    private String userName;
+
+    
+    private String userAccount;
+
+    
+    private String userProfile;
+
+    
+    private String userRole;
+
+    private static final long serialVersionUID = 1L;
+}
